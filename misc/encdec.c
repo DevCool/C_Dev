b930007f1356a0b7036d8192e22eaa194bc99a24
Philip Simonson <drunkenmaster1988@gmail.com>
2016-05-23 17:26:14 -0400

    Added support for passwords in encdec.

--- misc/encdec.c
+++ misc/encdec.c
@@ -8,17 +8,18 @@
 struct Header {
 	char tagHeader[3];
 	long size;
+	char tagPass[9];
 };
 
-void encrypt(FILE* fp, struct Header* fHead, const char* filename, const char* outname);
-void decrypt(FILE* fp, struct Header* fHead, const char* filename, const char* outname);
+void encrypt(FILE* fp, struct Header* fHead, const char* filename, const char* outname, const char* password);
+void decrypt(FILE* fp, struct Header* fHead, const char* filename, const char* outname, const char* password);
 
 int main(int argc, char* argv[]) {
 	FILE *fp = NULL;
 	struct Header fileHead;
 
-	if (argc < 4 || argc > 4) {
-		fprintf(stderr, "Usage: %s -[ed] filename.in filename.out\n",
+	if (argc < 4 || argc > 5) {
+		fprintf(stderr, "Usage: %s -[ed] file.in file.out [password 8 chars]\n",
 			argv[0]);
 		exit(1);
 	}
@@ -26,22 +27,34 @@ int main(int argc, char* argv[]) {
 	memset(&fileHead, 0, sizeof(struct Header));
 	if (argv[1][0] == '-') {
 		if (argv[1][1] == 'e') {
-			encrypt(fp, &fileHead, argv[2], argv[3]);
+			if (argc == 5) {
+				char password[9];
+				strncpy(password, argv[4], 8);
+				encrypt(fp, &fileHead, argv[2], argv[3], password);
+			} else {
+				encrypt(fp, &fileHead, argv[2], argv[3], "");
+			}
 		} else if (argv[1][1] == 'd') {
-			decrypt(fp, &fileHead, argv[2], argv[3]);
+			if (argc == 5) {
+				char password[9];
+				strncpy(password, argv[4], 8);
+				decrypt(fp, &fileHead, argv[2], argv[3], password);
+			} else {
+				decrypt(fp, &fileHead, argv[2], argv[3], "");
+			}
 		} else {
 			fprintf(stderr,
-				"Usage: %s -[ed] file.in file.out\n",
+				"Usage: %s -[ed] file.in file.out [password 8 chars]\n",
 				argv[0]);
 			exit(1);
 		}
 	} else {
-		fprintf(stderr, "Usage: %s -[ed] file.in file.out\n",
+		fprintf(stderr, "Usage: %s -[ed] file.in file.out [password 8 chars]\n",
 			argv[0]);
 	}
 }
 
-void encrypt(FILE* fp, struct Header* fHead, const char* filename, const char* outname) {
+void encrypt(FILE* fp, struct Header* fHead, const char* filename, const char* outname, const char* password) {
 	FILE *fout = NULL;
 	char *fname = strdup(filename);
 	char *fname2 = strdup(outname);
@@ -68,8 +81,13 @@ void encrypt(FILE* fp, struct Header* fHead, const char* filename, const char* o
 	size = ftell(fp);
 	rewind(fp);
 
+	memset(fHead->tagHeader, 0, 3);
+	memset(fHead->tagPass, 0, 9);
+
 	strncpy(fHead->tagHeader, "PRS", 3);
 	fHead->size = (size+sizeof(struct Header));
+	strncpy(fHead->tagPass, password, 8);
+	fHead->tagPass[9] = '\0';
 
 	int rc = fwrite(fHead, sizeof(struct Header), 1, fout);
 	if (rc != 1) {
@@ -97,7 +115,7 @@ void encrypt(FILE* fp, struct Header* fHead, const char* filename, const char* o
 	free(fname);
 }
 
-void decrypt(FILE* fp, struct Header* fHead, const char* filename, const char* outname) {
+void decrypt(FILE* fp, struct Header* fHead, const char* filename, const char* outname, const char* password) {
 	FILE *fout = NULL;
 	char *fname = strdup(filename);
 	char *fname2 = strdup(outname);
@@ -112,14 +130,6 @@ void decrypt(FILE* fp, struct Header* fHead, const char* filename, const char* o
 		exit(1);
 	}
 
-	if ((fout = fopen(fname2, "wb")) == NULL) {
-		fprintf(stderr, "Error: Cannot open file %s.\n", fname2);
-		fclose(fp);
-		free(fname2);
-		free(fname);
-		exit(1);
-	}
-
 	fseek(fp, 0, SEEK_END);
 	size = ftell(fp);
 	rewind(fp);
@@ -127,7 +137,6 @@ void decrypt(FILE* fp, struct Header* fHead, const char* filename, const char* o
 	int rc = fread(fHead, sizeof(struct Header), 1, fp);
 	if (rc != 1) {
 		fprintf(stderr, "Error: File format not recognized!\n");
-		fclose(fout);
 		fclose(fp);
 		free(fname2);
 		free(fname);
@@ -136,7 +145,6 @@ void decrypt(FILE* fp, struct Header* fHead, const char* filename, const char* o
 
 	if (strncmp(fHead->tagHeader, "PRS", 3) != 0) {
 		fprintf(stderr, "Error: File Header invalid.\n");
-		fclose(fout);
 		fclose(fp);
 		free(fname2);
 		free(fname);
@@ -145,7 +153,22 @@ void decrypt(FILE* fp, struct Header* fHead, const char* filename, const char* o
 
 	if (size != fHead->size) {
 		fprintf(stderr, "Error: File size is invalid.\n");
-		fclose(fout);
+		fclose(fp);
+		free(fname2);
+		free(fname);
+		exit(1);
+	}
+
+	if (strncmp(fHead->tagPass, password, 8) != 0) {
+		fprintf(stderr, "Error: Password doesn't match!\n");
+		fclose(fp);
+		free(fname2);
+		free(fname);
+		exit(1);
+	}
+
+	if ((fout = fopen(fname2, "wb")) == NULL) {
+		fprintf(stderr, "Error: Cannot open file %s.\n", fname2);
 		fclose(fp);
 		free(fname2);
 		free(fname);
