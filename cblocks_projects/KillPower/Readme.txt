56ebe0af6ccfa95b26c200d62848dae80c503623
Philip R. Simonson <drunkenmaster1988@gmail.com>
2017-05-01 05:58:08 -0400

    Added KillPower, shutdown multiple computers on network.

---
+++ cblocks_projects/KillPower/Readme.txt
@@ -0,0 +1,12 @@
+===================
+= KillPower v0.01 =
+===================
+
+For windows/linux.
+
+Shutdown multiple computers with one
+computer. Just run this in the background
+on multiple computers and use another
+computer to connect to them. As soon
+as you are connected to it, that computer
+you connected to should shutdown.
---
+++ cblocks_projects/KillPower/main.c
@@ -0,0 +1,99 @@
+/******************************************************
+ * A program to poweroff multiple computers across
+ * a network.
+ ******************************************************
+ */
+
+#if defined(__linux__)
+	#include <sys/socket.h>
+	#include <arpa/inet.h>
+#else
+	#include <windows.h>
+	#include <winsock2.h>
+#endif
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#define error(T) if((T)){ fprintf(stderr, "%s\n", strerror(errno)); errno = 0; goto error; }
+
+DWORD (*funcThread)(LPVOID lpVoid);
+
+DWORD prog_thread(LPVOID lpVoid)
+{
+#if defined(_WIN32)
+    char *pname = "C:\\Windows\\system32\\shutdown.exe";
+    const char *args[] = {"/p", NULL};
+#else
+	char *pname = "/sbin/shutdown";
+	const char *args[] = {"-P -h", NULL};
+#endif
+	execvp(pname, args);
+	return 0;
+}
+
+int main()
+{
+	HANDLE hThread;
+	struct sockaddr_in self;
+	int sockfd;
+
+#if defined(_WIN32)
+	WSADATA wsaData;
+	ZeroMemory(&wsaData, sizeof(wsaData));
+	WSAStartup(MAKEWORD(2, 0), &wsaData);
+#endif
+
+	if((sockfd=socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+		perror("socket()");
+		WSACleanup();
+		exit(1);
+	}
+
+	ZeroMemory(&self, sizeof(self));
+	self.sin_family = AF_INET;
+	self.sin_port = htons(8888);
+	self.sin_addr.s_addr = INADDR_ANY;
+
+    error(bind(sockfd, (struct sockaddr*)&self, sizeof(self)) < 0);
+    error(listen(sockfd, 20) < 0);
+
+    while(1) {
+        struct sockaddr_in client;
+        int clientlen = sizeof(client);
+        int clientfd;
+		clientfd=accept(sockfd, (struct sockaddr*)&client, &clientlen);
+		if(clientfd > 0) {
+#if defined(_WIN32)
+			funcThread = &prog_thread;
+			funcThread(NULL);
+			closesocket(clientfd);
+#else
+			funcThread = &prog_thread;
+			funcThread(NULL);
+			close(clientfd);
+#endif
+		} else
+			perror("accept()");
+    }
+
+#if defined(_WIN32)
+	while(WaitForSingleObject(hThread, 0) == 0);
+	closesocket(sockfd);
+	WSACleanup();
+#else
+	close(sockfd);
+#endif
+    return 0;
+
+error:
+#if defined(_WIN32)
+	closesocket(sockfd);
+	WSACleanup();
+#else
+	close(sockfd);
+#endif
+	return 1;
+}
