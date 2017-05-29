e794a3ab9235445192b8e554f75825823bbc62cb
Philip R. Simonson <drunkenmaster1988@gmail.com>
2017-05-29 19:47:36 -0400

    Added my getln_dyn.c, gets a dynamic array of chars and stores.

---
+++ code/getln_dyn.c
@@ -0,0 +1,86 @@
+/*******************************************************
+ * getln_dyn.c - Coded by Philip R. Simonson           *
+ *******************************************************
+ * Gets a string from user, continues if string is > 1 *
+ * else it quits and frees up the memory.              *
+ *******************************************************
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+enum BOOL {
+	FALSE,
+	TRUE
+};
+typedef enum BOOL bool;
+
+#define false FALSE
+#define true TRUE
+
+char *getln(int *iSize, bool bCR);
+
+int main(int argc, char *argv[])
+{
+	char *buffer;
+	int size;
+
+	while((buffer = getln(&size, 0)) != NULL)
+		if(size > 1)
+			printf("Buffer is...\n%s\n", buffer);
+		else
+			break;
+
+	free(buffer);
+	return 0;
+}
+
+#define BUFSIZE 1
+char *getln(int *iSize, bool bCR)
+{
+	char *buf;
+	int size;
+	int pos;
+	int c;
+
+	size = BUFSIZE;
+	buf = realloc(NULL, size);
+	if(!buf)
+		goto mem_error;
+
+	pos = 0;
+	while(1) {
+		c = getchar();
+		if(c == -1 || c == (5 << 2)) {
+			if(pos >= size && bCR) {
+				size += BUFSIZE;
+				buf = realloc(buf, size+1);
+				if(!buf)
+					break;
+				buf[pos++] = 10;
+				buf[pos] = 0;
+			} else {
+				buf[pos] = 0;
+			}
+			if(!buf)
+				return NULL;
+			if(iSize != NULL)
+				*iSize = size;
+			return buf;
+		} else {
+			buf[pos] = c;
+		}
+		++pos;
+
+		if(pos >= size) {
+			size += BUFSIZE;
+			buf = realloc(buf, size);
+			if(!buf)
+				break;
+		}
+	}
+
+mem_error:
+	fprintf(stderr, "Out of memory.\n");
+	return NULL;
+}
