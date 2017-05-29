d839a677eac1ed7f11330b5fd70ed84c39a5c81d
Philip R. Simonson <drunkenmaster1988@gmail.com>
2017-05-29 11:35:00 -0400

    Fixed my createprocess function in logon.c

--- code/killpower2/logon.c
+++ code/killpower2/logon.c
@@ -41,37 +41,16 @@ BOOL SetTokenPrivilege(HANDLE hToken, LPCSTR szPrivilege, BOOL bEnablePrivilege)
 
 BOOL CreateRemoteProcess(char *username, const char *domain, const char *password, const char *app)
 {
-	DWORD dwSize;
 	HANDLE hToken;
-	LPVOID lpvEnv;
+	HANDLE hPrimaryToken;
 	PROCESS_INFORMATION pi;
-	PROFILEINFOW profile;
-	STARTUPINFOW si;
-	WCHAR szUserProfile[256];
-	WCHAR szUser[128];
-	WCHAR szPass[128];
-	WCHAR szDomain[128];
-	WCHAR szApp[128];
-	WCHAR szPath[256];
+	STARTUPINFO si;
+	SECURITY_ATTRIBUTES sa;
+	char szPath[256];
 
-	ZeroMemory(&profile, sizeof(profile));
-	ZeroMemory(szUserProfile, sizeof(szUserProfile)/sizeof(WCHAR));
-	ZeroMemory(szUser, sizeof(szUser)/sizeof(WCHAR));
-	ZeroMemory(szPass, sizeof(szPass)/sizeof(WCHAR));
-	ZeroMemory(szDomain, sizeof(szDomain)/sizeof(WCHAR));
-	ZeroMemory(szApp, sizeof(szApp)/sizeof(WCHAR));
 	ZeroMemory(&si, sizeof(si));
 	ZeroMemory(&pi, sizeof(pi));
 	si.cb = sizeof(si);
-
-	if(!MByteToUnicode(username, szUser, sizeof(szUser)/sizeof(WCHAR)))
-		return FALSE;
-	if(!MByteToUnicode(password, szPass, sizeof(szPass)/sizeof(WCHAR)))
-		return FALSE;
-	if(!MByteToUnicode(domain, szDomain, sizeof(szDomain)/sizeof(WCHAR)))
-		return FALSE;
-	if(!MByteToUnicode(app, szApp, sizeof(szApp)/sizeof(WCHAR)))
-		return FALSE;
 
 	if(!LogonUser(username, domain, password, LOGON32_LOGON_INTERACTIVE,
 			LOGON32_PROVIDER_DEFAULT, &hToken)) {
@@ -80,53 +59,37 @@ BOOL CreateRemoteProcess(char *username, const char *domain, const char *passwor
 		return FALSE;
 	}
 
-	if(!CreateEnvironmentBlock(&lpvEnv, hToken, TRUE)) {
-		fprintf(stderr, "Error: CreateEnvBlock() function failed.\nError code %lu\n",
+	ZeroMemory(&sa, sizeof(sa));
+	sa.nLength = sizeof(SECURITY_ATTRIBUTES);
+	if(!DuplicateTokenEx(hToken, MAXIMUM_ALLOWED, &sa,
+			SecurityIdentification, TokenPrimary, &hPrimaryToken)) {
+		fprintf(stderr, "Error: DuplicateToken() function failed.\nError code %lu\n",
+			GetLastError());
+		return FALSE;
+	}
+	CloseHandle(hToken);
+
+	if(!ImpersonateLoggedOnUser(hPrimaryToken)) {
+		fprintf(stderr, "Error: ImpersonateLoggedOnUser() function failed.\nError code %lu\n",
 			GetLastError());
 		return FALSE;
 	}
 
-	/* Load User Profile */
-	profile.dwSize = sizeof(profile);
-	profile.lpUserName = szUser;
-	if(!LoadUserProfileW(hToken, &profile)) {
-		fprintf(stderr, "Error: LoadUserProfile() function failed.\nError code %lu\n",
-			GetLastError());
-		return FALSE;
-	}
-
-	/* Get User Profile Directory */
-	dwSize = sizeof(szUserProfile)/sizeof(WCHAR);
-	if(!GetUserProfileDirectoryW(hToken, szUserProfile, &dwSize)) {
-		fprintf(stderr, "Error: GetUserProfileDirectory() function failed.\nError code %lu\n",
-			GetLastError());
-		return FALSE;
-	}
-
-	ZeroMemory(szPath, sizeof(szPath)/sizeof(WCHAR));
-	GetSystemDirectoryW(szPath, sizeof(szPath)/sizeof(WCHAR));
+	ZeroMemory(szPath, sizeof(szPath)/sizeof(char));
+	GetSystemDirectory(szPath, sizeof(szPath)/sizeof(char));
 
 	si.cb = sizeof(STARTUPINFOW);
 	si.dwFlags = STARTF_USESHOWWINDOW;
 	si.wShowWindow = SW_SHOWNORMAL;
-	if(!CreateProcessWithLogonW(szUser, szDomain, szPass,
-			LOGON_WITH_PROFILE, szApp, NULL, CREATE_NEW_CONSOLE | CREATE_UNICODE_ENVIRONMENT,
-			lpvEnv, szPath, &si, &pi)) {
+	if(!CreateProcessAsUser(hPrimaryToken, app, NULL, 
+			&sa, &sa, FALSE, CREATE_NEW_CONSOLE | CREATE_UNICODE_ENVIRONMENT,
+			NULL, szPath, &si, &pi)) {
 		fprintf(stderr, "Error: Cannot create the process.\nError code %lu\n",
 			GetLastError());
 		return FALSE;
 	}
 
-	if(!DestroyEnvironmentBlock(lpvEnv)) {
-		fprintf(stderr, "Error: Cannot destroy environment block.\nError code %lu\n",
-			GetLastError());
-		CloseHandle(hToken);
-		CloseHandle(pi.hProcess);
-		CloseHandle(pi.hThread);
-		return FALSE;
-	}
-
-	CloseHandle(hToken);
+	CloseHandle(hPrimaryToken);
 	CloseHandle(pi.hProcess);
 	CloseHandle(pi.hThread);
 
