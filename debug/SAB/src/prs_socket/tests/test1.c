0bac74bdc8cfe88990e2ed4f55c4edf1833bc3f0
Philip R. Simonson <drunkenmaster1988@gmail.com>
2017-07-16 16:13:19 -0400

    Added tests in prs_socket.

---
+++ debug/SAB/src/prs_socket/tests/test1.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+
+/* #include <prs_socket/socket.h> */
+
+int main(void) {
+  /*  sockcreate_func_t sock_func;
+  struct sockaddr_in client;
+  int sockfd, clientfd, retval;*/
+
+  /* initialize socket func */
+  //  socket_init(SOCKET_BIND, &sock_func);
+  /* bind socket to port */
+  //  sockfd = sock_func.socket_bind("0.0.0.0", 5555, &clientfd, &client);
+  /* handle server (swap to client handler) */
+  //  retval = handle_server(&sockfd, &clientfd, &client, NULL, NULL);
+  /* cleanup socket */
+  //  close_socket(&sockfd);
+  return 0;
+}
