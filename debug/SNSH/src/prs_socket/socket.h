44f175e9ded9538dbb355e900c9e4a13b37e8732
Philip R. Simonson <drunkenmaster1988@gmail.com>
2017-07-27 14:58:05 -0400

    Fixed socket.h in prs_socket, now works in windows and linux.

--- debug/SNSH/src/prs_socket/socket.h
+++ debug/SNSH/src/prs_socket/socket.h
@@ -16,6 +16,7 @@
 #include <netdb.h>
 #endif
 
+#if defined(_WIN32) || (_WIN64)
 /* my socket enumeration */
 DLL_EXPORT enum SOCKCREATE {
   SOCKET_BIND,
@@ -31,7 +32,6 @@ DLL_EXPORT union SOCKET_CREATE {
 typedef DLL_EXPORT union SOCKET_CREATE sockcreate_func_t;
 
 /* my function prototypes */
-#if defined(_WIN32) || (_WIN64)
 int DLL_EXPORT socket_init(sockcreate_t init, sockcreate_func_t *socket_func);
 int DLL_EXPORT create_conn(const char *hostname, int port, int *clientfd, struct sockaddr_in *clientaddr);
 int DLL_EXPORT create_bind(const char *hostname, int port, int *clientfd, struct sockaddr_in *clientaddr);
@@ -41,6 +41,20 @@ int DLL_EXPORT handle_server(int *sockfd, int *clientfd, struct sockaddr_in *cli
 				    const char *filename));
 int DLL_EXPORT handle_client(int *sockfd, struct sockaddr_in *client, const char *filename);
 #else
+/* sockcreate_t enum */
+enum SOCKCREATE {
+  SOCKET_BIND,
+  SOCKET_CONN
+};
+typedef enum SOCKCREATE sockcreate_t;
+
+/* socketcreate_t union */
+union SOCKET_CREATE {
+  int (*socket_bind)(const char *, int, int *, struct sockaddr_in *);
+  int (*socket_conn)(const char *, int, int *, struct sockaddr_in *);
+};
+typedef union SOCKET_CREATE sockcreate_func_t;
+
 int socket_init(sockcreate_t init, sockcreate_func_t *socket_func);
 int create_conn(const char *hostname, int port, int *clientfd, struct sockaddr_in *clientaddr);
 int create_bind(const char *hostname, int port, int *clientfd, struct sockaddr_in *clientaddr);
