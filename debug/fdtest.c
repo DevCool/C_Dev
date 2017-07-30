58f0634b54a9113968cd8fc57b5aa606eeaad3d9
Philip R. Simonson <drunkenmaster1988@gmail.com>
2017-07-30 16:30:54 -0400

    Updated fdtest.c, although untested.

--- debug/fdtest.c
+++ debug/fdtest.c
@@ -20,12 +20,14 @@ int main(void) {
   struct addrinfo hints, *res = NULL;
   struct sockaddr client;
   socklen_t addrlen;
-  int sockfd, clientfd, rv;
+  int sockfd, clientfd, rv, i;
+  int fdmax = 0;
   char entry[9];
 #ifdef __linux
-  fd_set read_fds;
+  fd_set master, read_fds;
 #elif _WIN32 || _WIN64
-  FD_SET read_fds;
+  FD_SET master, read_fds;
+  u_long on = 1;
 #endif
 #if defined(_WIN32) || (_WIN64)
   WSADATA wsaData;
@@ -50,6 +52,18 @@ int main(void) {
     goto error;
   }
 
+#if defined(_WIN32) || (_WIN64)
+  if(ioctlsocket((SOCKET)sockfd, FIONBIO, &on) != 0) {
+    perror("ioctlsocket");
+    goto error;
+  }
+#else
+  if(fcntl(sockfd, F_SETFL, O_NONBLOCK) < 0) {
+    perror("fcntl");
+    goto error;
+  }
+#endif
+
   if(bind(sockfd, res->ai_addr, res->ai_addrlen) < 0) {
     perror("bind");
     goto error;
@@ -60,38 +74,56 @@ int main(void) {
     goto error;
   }
 
+  FD_ZERO(&master);
+  FD_ZERO(&read_fds);
+  FD_SET(sockfd, &master);
+  fdmax = sockfd;
+  
   while(1) {
-    FD_ZERO(&read_fds);
-    FD_SET(sockfd, &read_fds);
-    select(sockfd+1, &read_fds, NULL, NULL, NULL);
-    if(FD_ISSET(clientfd, &read_fds)) {
-      clientfd = accept(sockfd, &client, &addrlen);
-      if(clientfd < 0) {
-	perror("accept");
-	break;
-      } else {
-	FD_SET(clientfd, &read_fds);
+    read_fds = master;
+    select(fdmax+1, &read_fds, NULL, NULL, NULL);
+
+    for(i = 0; i < fdmax; i++) {
+      if(FD_ISSET(i, &read_fds)) {
+        if(i == sockfd) { /* get a new connection */
+          clientfd = accept(sockfd, &client, &addrlen);
+          if(clientfd < 0) {
+	    perror("accept");
+          } else {
+	    FD_SET(clientfd, &read_fds);
+	    if(clientfd > fdmax)
+	      fdmax = clientfd;
+          }
+	} else { /* handle data from client */
+          do {
+	    memset(entry, 0, sizeof entry);
+	    send(i, "Enter password: ", 16, 0);
+            recv(i, entry, sizeof entry, 0);
+          } while(strncmp(entry, "AW96B6", sizeof entry) != 0);
+#if defined(_WIN32) || (_WIN64)
+          closesocket((SOCKET)i);
+#else
+          close(i);
+#endif
+          break;
+	}
       }
-    } else {
-      do {
-	memset(entry, 0, sizeof entry);
-	send(clientfd, "Enter password: ", 16, 0);
-	recv(clientfd, entry, sizeof entry, 0);
-      } while(strncmp(entry, "AW96B6", sizeof entry) != 0);
-      close(clientfd);
-      break;
     }
   }
-  close(sockfd);
 #if defined(_WIN32) || (_WIN64)
+  closesocket((SOCKET)sockfd);
   WSACleanup();
+#else
+  close(sockfd);
 #endif
   return 0;
 
  error:
-  close(sockfd);
 #if defined(_WIN32) || (_WIN64)
+  closesocket((SOCKET)sockfd);
   WSACleanup();
+#else
+  close(sockfd);
 #endif
   return 1;
 }
