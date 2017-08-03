b7730a9bfa271ce18b6b01f4e3c09803a308cc82
Philip R. Simonson <drunkenmaster1988@gmail.com>
2017-08-03 19:09:11 -0400

    Removed SNSH from here, now in C_Projects

--- SNSH/src/espeech.h
+++
@@ -1,9 +0,0 @@
-#ifndef espeech_h
-#define espeech_h
-
-int SynthCallback(short *wav, int numsamples, espeak_EVENT *events);
-int speakInit(void);
-void speakCleanup(void);
-int speak(char *text, int len);
-
-#endif
--- SNSH/src/transfer.h
+++
@@ -1,9 +0,0 @@
-#ifndef transfer_h
-#define transfer_h
-
-int download(const char *hostname, const char *filename);
-int upload(const char *hostname, const char *filename);
-int hdl_download(int *sockfd, struct sockaddr_in *client, const char *filename);
-int hdl_upload(int *sockfd, struct sockaddr_in *client, const char *filename);
-
-#endif
