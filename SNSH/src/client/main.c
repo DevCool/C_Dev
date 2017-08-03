f1fe754e05fc6fa7b61bdc140293467e8260c633
Philip R. Simonson <drunkenmaster1988@gmail.com>
2017-08-02 20:36:44 -0400

    Updated my client a little.

--- SNSH/src/client/main.c
+++ SNSH/src/client/main.c
@@ -9,6 +9,9 @@
 #include <stdlib.h>
 #include <string.h>
 #include <errno.h>
+#ifdef __linux
+#include <fcntl.h>
+#endif
 
 #include "../debug.h"
 #include "../prs_socket/socket.h"
@@ -19,6 +22,11 @@ int main(int argc, char *argv[]) {
   sockcreate_func_t sockfunc;
   struct sockaddr_in client;
   int sockfd, clientfd, retval;
+#if defined(_WIN32) || (_WIN64)
+  u_long on = 0;
+#else
+  int on = 0;
+#endif
 
   if(argc < 2 || argc > 3) {
     printf("Usage: %s <ipaddress> [port]\n", argv[0]);
@@ -29,15 +37,25 @@ int main(int argc, char *argv[]) {
     ERROR_FIXED(socket_init(SOCKET_CONN, &sockfunc) < 0, "socket init failed.\n");
     ERROR_FIXED((sockfd = create_conn(argv[1], 8888, &clientfd, &client)) < 0,
 		"Could not create socket.\n");
+#if defined(_WIN32) || (_WIN64)
+    ioctlsocket(sockfd, FIONBIO, &on);
+#else
+    fcntl(sockfd, F_SETFL, &on);
+#endif
     ERROR_FIXED(handle_server(&sockfd, &clientfd, &client, NULL, &hdl_client) < 0,
-		"Could handle server.\n");
+		"Could not handle server.\n");
     retval = hdl_client(&sockfd, &client, NULL);
   } else {
-    ERROR_FIXED(socket_init(SOCKET_CONN, &sockfunc) < 0, "socket init failed.\n");
+    ERROR_FIXED(socket_init(SOCKET_CONN, &sockfunc) < 0, "Socket init failed.\n");
     ERROR_FIXED((sockfd = create_conn(argv[1], atoi(argv[2]), &clientfd, &client)) < 0,
 		"Could not create socket.\n");
+#if defined(_WIN32) || (_WIN64)
+    ioctlsocket(sockfd, FIONBIO, &on);
+#else
+    fcntl(sockfd, F_SETFL, &on);
+#endif
     ERROR_FIXED(handle_server(&sockfd, &clientfd, &client, NULL, &hdl_client) < 0,
-		"Could handle server.\n");
+		"Could not handle server.\n");
     retval = hdl_client(&sockfd, &client, NULL);
   }
   close_socket(&sockfd);
@@ -71,11 +89,6 @@ int hdl_client(int *sockfd, struct sockaddr_in *client, const char *filename) {
     FD_SET(*sockfd, &wfds);
     ERROR_FIXED((ret = select(*sockfd + 1, &rfds, &wfds, NULL, &tv)) < 0,
 		"Select error occured.\n");
-    if(ret == 0) {
-      continue;
-    } else if(ret == -1) {
-      break;
-    }
     
     if(FD_ISSET(*sockfd, &rfds)) {
       memset(msg, 0, sizeof msg);
